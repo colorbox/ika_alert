d7183df19681965f9f81cc2ae2e1a7606f8450ca
colorbox <colorbox222@gmail.com>
2018-08-07 23:52:38 +0900

    Update triming script for dir place

--- ika_triming.rb
+++ ika_triming.rb
@@ -1,39 +1,48 @@
+require 'fileutils'
 require 'RMagick'
 require 'rubygems'
 
 
-def crop_icons(filename)
+def crop_icons(filename:, distdir:)
   original = Magick::Image.read(filename).first
-  prefix = File.basename(filename, ".*")
+  prefix = distdir.gsub('/','_') + File.basename(filename, ".*")
+
+  puts(prefix)
 
   image = original.crop(356, 15, 53, 60)
-  image.write("icons/#{prefix}-1.jpg")
+  image.write("#{distdir}/#{prefix}-1.jpg")
 
   image = original.crop(356 + 53 + 5, 15, 53, 60)
-  image.write("icons/#{prefix}-2.jpg")
+  image.write("#{distdir}/#{prefix}-2.jpg")
 
   image = original.crop(356 + (53 + 5) * 2, 15, 53, 60)
-  image.write("icons/#{prefix}-3.jpg")
+  image.write("#{distdir}/#{prefix}-3.jpg")
 
   image = original.crop(356 + (53 + 5) * 3 - 1, 15, 53, 60)
-  image.write("icons/#{prefix}-4.jpg")
+  image.write("#{distdir}/#{prefix}-4.jpg")
 
   image = original.crop(356 + (53 + 5) * 3 + 167, 15, 53, 60)
-  image.write("icons/#{prefix}-5.jpg")
+  image.write("#{distdir}/#{prefix}-5.jpg")
 
   image = original.crop(356 + (53 + 5) * (3+1) + 167, 15, 53, 60)
-  image.write("icons/#{prefix}-6.jpg")
+  image.write("#{distdir}/#{prefix}-6.jpg")
 
   image = original.crop(356 + (53 + 5) * (3+2) + 167, 15, 53, 60)
-  image.write("icons/#{prefix}-7.jpg")
+  image.write("#{distdir}/#{prefix}-7.jpg")
 
   image = original.crop(356 + (53 + 5) * (3+3) + 167 - 1, 15, 53, 60)
-  image.write("icons/#{prefix}-8.jpg")
-
+  image.write("#{distdir}/#{prefix}-8.jpg")
 end
 
-Dir.entries("./douga").each do |filename|
+sliced_images_dirname = ARGV[0]
+raise('you need image directory with args') if sliced_images_dirname.nil?
+movie_name = sliced_images_dirname.split('/').last
+dist_dir = "data/icons/#{movie_name}"
+FileUtils.mkdir_p dist_dir
+
+Dir.entries(sliced_images_dirname).each do |filename|
   next unless File.extname(filename)==".jpg"
-  puts "./douga/#{filename}"
-  crop_icons("./douga/#{filename}")
+  filepath = "#{sliced_images_dirname}/#{filename}"
+  puts filepath
+  crop_icons(filename: filepath ,distdir: dist_dir)
 end
