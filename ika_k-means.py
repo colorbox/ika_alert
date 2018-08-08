2050952629b392e46b203ecdca75dcee29683818
colorbox <colorbox222@gmail.com>
2018-08-08 22:09:54 +0900

    Update k-means clustering script more useful

--- ika_k-means.py
+++ ika_k-means.py
@@ -5,13 +5,22 @@ from PIL import Image
 from skimage import data
 from sklearn.cluster import KMeans
 
+print('clustering start')
+
 cluster_count = 4
 
-origin_dir = './data/merged_icons/'
+origin_dir = './data/icons/'
 dist_dir = './data/grouped_icons/'
-origin_images =  [path for path in os.listdir(origin_dir) if not path.startswith('.')]
 
-feature = np.array([data.imread(origin_dir + path) for path in origin_images])
+origin_dirs = [(origin_dir + path) for path in os.listdir(origin_dir) if not path.startswith('.')]
+
+# read all icons in origin_dir
+origin_images = []
+for dir in origin_dirs:
+    for path in os.listdir(dir):
+        origin_images.append(dir + '/' + path)
+
+feature = np.array([data.imread(path) for path in origin_images])
 feature = feature.reshape(len(feature), -1).astype(np.float64)
 
 print('data reshape completed')
@@ -22,12 +31,12 @@ print('clustering completed')
 
 labels = model.labels_
 
-print(labels)
-
 for label, path in zip(labels, origin_images):
+    image_name = path.split('/')[-1]
     dirpath = dist_dir + str(label)
     if not os.path.isdir(dirpath):
         os.makedirs(dirpath)
-    shutil.copyfile(origin_dir + path, dist_dir + str(label) + "/" + path)
+
+    shutil.copyfile(path, dist_dir + str(label) + "/" + image_name)
 
 print('all completed')
