799745683e35e0a1b724fbaa2ab9e37cc55bf467
colorbox <colorbox222@gmail.com>
2018-09-12 23:33:46 +0900

    Update pinch detector

--- pinch_detector.py
+++ pinch_detector.py
@@ -1,23 +1,34 @@
+# -*- coding: utf-8 -*
+
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
-# img = cv.imread('pinch.jpg',0)
-img = cv.imread('no_pinch.jpg',0)
+img = cv.imread('friend_pinch.jpg',0)
+# img = cv.imread('enemy_pinch.jpg',0)
+# img = cv.imread('no_pinch.jpg',0)
+
+template = cv.imread('enemy_small_croped_pinch.png',0)
+# template = cv.imread('friend_small_croped_pinch.png',0)
+
 img2 = img.copy()
-template = cv.imread('small_croped.png',0)
 w, h = template.shape[::-1]
-# All the 6 methods for comparison in a list
-methods = ['cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
-            'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED']
+
+methods = ['cv.TM_CCOEFF_NORMED']
+
 for meth in methods:
     img = img2.copy()
     method = eval(meth)
     # Apply template Matching
     res = cv.matchTemplate(img,template,method)
     min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+    if max_val > 0.95:
+        print("pinch!!!!")
+    else:
+        print("not pinch ＼(ツ)／")
+
     print(max_val)
-    print('-------------')
+
     # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
     if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
         top_left = min_loc
