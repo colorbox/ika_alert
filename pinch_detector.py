132a158a09b685e52cb64d555b416ae18f6f9660
colorbox <colorbox222@gmail.com>
2018-09-13 23:23:25 +0900

    Update pinch detector trial simple

--- pinch_detector.py
+++ pinch_detector.py
@@ -4,41 +4,34 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv.imread('friend_pinch.jpg',0)
-# img = cv.imread('enemy_pinch.jpg',0)
-# img = cv.imread('no_pinch.jpg',0)
+img = cv.imread('data/pinch/000217.jpg',0)
 
-template = cv.imread('enemy_small_croped_pinch.png',0)
-# template = cv.imread('friend_small_croped_pinch.png',0)
+template = cv.imread('enemy_pinch2.png',0)
+template = cv.imread('friend_pinch1.png',0)
 
-img2 = img.copy()
 w, h = template.shape[::-1]
 
-methods = ['cv.TM_CCOEFF_NORMED']
+method = cv.TM_CCOEFF_NORMED
+# Apply template Matching
+res = cv.matchTemplate(img,template,method)
+min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+if max_val > 0.95:
+    print("pinch!!!!")
+else:
+    print("not pinch ＼(ツ)／")
 
-for meth in methods:
-    img = img2.copy()
-    method = eval(meth)
-    # Apply template Matching
-    res = cv.matchTemplate(img,template,method)
-    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-    if max_val > 0.95:
-        print("pinch!!!!")
-    else:
-        print("not pinch ＼(ツ)／")
+print(max_val)
 
-    print(max_val)
-
-    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-    if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
-        top_left = min_loc
-    else:
-        top_left = max_loc
-    bottom_right = (top_left[0] + w, top_left[1] + h)
-    cv.rectangle(img,top_left, bottom_right, 255, 2)
-    plt.subplot(121),plt.imshow(res,cmap = 'gray')
-    plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122),plt.imshow(img,cmap = 'gray')
-    plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-    plt.suptitle(meth)
-    plt.show()
+# If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+if cv.TM_CCOEFF_NORMED in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
+    top_left = min_loc
+else:
+    top_left = max_loc
+bottom_right = (top_left[0] + w, top_left[1] + h)
+cv.rectangle(img,top_left, bottom_right, 255, 2)
+plt.subplot(121),plt.imshow(res,cmap = 'gray')
+plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(img,cmap = 'gray')
+plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+plt.suptitle('cv.TM_CCOEFF_NORMED')
+plt.show()
